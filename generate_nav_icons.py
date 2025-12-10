d4c99282519d7366976e87c1e1c698edba2f7a77
Alex Anahart <alex.anahart@example.com>
2025-12-10 14:33:43 +0100

    щл

--- generate_nav_icons.py
+++ generate_nav_icons.py
@@ -5,7 +5,7 @@ import os
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Путь к файлу HTML в той же папке
-html_file = os.path.join(current_dir, "wear_01.html")
+html_file = os.path.join(current_dir, "wear_03.html")
 
 # Путь к папке с изображениями в той же папке
 images_root = os.path.join(current_dir, "images")
